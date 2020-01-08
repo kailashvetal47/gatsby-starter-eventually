e48d2a869f85ba3135eb21e6c63c76e2a0c6f8a5
Kailash Vetal <Kailash.VETAL@3ds.com>
2020-01-08 16:46:54 +0100

    Updates information fields

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
+  siteTitle: 'Kailash Vetal', // <title>
   manifestName: 'Eventually',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -9,9 +9,9 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Eventually',
+  heading: 'Kailash Vetal',
   subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
+    "Geek | Gamer | 9gagger",
   socialLinks: [
     {
       icon: 'fa-github',
@@ -31,7 +31,7 @@ module.exports = {
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:kailash.vetal47@gmail.com',
     },
   ],
 };
